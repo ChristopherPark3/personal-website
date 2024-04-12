6cf3fd455e0bf718931b21225b5db8e83d7a188e
Chris Park <cp3063@columbia.edu>
2024-04-12 15:53:24 -0700

    basic styling for nav

--- src/app/components/V2/Navbar/AceternityNavbar.tsx
+++ src/app/components/V2/Navbar/AceternityNavbar.tsx
@@ -50,17 +50,17 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border-2 border-neutral-700 rounded-full  bg-neutral-900 shadow-sm shadow-gray-700 z-[5000] pr-2 pl-2 py-2  items-center justify-center space-x-4",
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border-2 border-neutral-700 rounded-full  bg-neutral-900 shadow-sm shadow-gray-700 z-[5000] pr-4 pl-2 py-2  items-center justify-center space-x-6 ",
           className
         )}
       >
-        <button className="border text-sm font-medium relative border-neutral-200  dark:text-white px-4 py-2 rounded-full hover:cursor-pointer hover:text-neutral-200 text-neutral-400">
+        <button className="text-sm font-medium relative pl-4  py-2 rounded-full hover:cursor-pointer hover:text-white text-gray-300">
           All
         </button>
         {navItems.map((navItem: any, idx: number) => (
           <h1
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-400  hover:text-neutral-200 text-sm hover:cursor-pointer"
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-gray-300  hover:text-white text-sm hover:cursor-pointer"
             )}
             key={idx}
           >
