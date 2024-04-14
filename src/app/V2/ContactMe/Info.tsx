12f8e8edd4c6bf6407b259408bbac7f089410494
Chris Park <cp3063@columbia.edu>
2024-04-13 19:02:41 -0700

    Almost done with ui for contact me. Message button needs to be adjusted.

---
+++ src/app/V2/ContactMe/Info.tsx
@@ -0,0 +1,31 @@
+import info from "../../../../public/info.svg";
+import Image from "next/image";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../../components/Tooltip";
+
+export default function Info() {
+  return (
+    <div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Image
+              src={info}
+              width={0}
+              height={0}
+              alt="info button"
+              className="w-4 h-4  "
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-neutral-800 border-2 border-gray-200">
+            <p >Just asking so I know who is who :{')'}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}
