ee57840af6d50cff9ecd80d31ab50a6e92c194e5
Chris Park <cp3063@columbia.edu>
2024-04-13 18:27:27 -0700

    Finalizes about me content

--- src/app/V2/V2AboutMe.tsx
+++ src/app/V2/V2AboutMe.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 export default function V2AboutMe() {
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-3xl text-gray-200">About me</h1>
+    <div className="flex flex-col gap-3 text-sm">
+      <h1 className="text-2xl text-gray-200">About me</h1>
       <p className="text-gray-200">
         I'm a software engineer with a knack for solving problems with software.
         I have experience working with Next.js, TypeScript, Node.js, and more.
@@ -11,9 +11,9 @@ export default function V2AboutMe() {
       </p>
       <p>
         My journey has been all over the place, from finance, to dentistry, but
-        ultimately I found my passion in software engineering. I'm a firm
-        believer that it's never too late to start something new, and I'm
-        very excited to see where this journey takes me. 
+        ultimately I found my passion in software engineering. Being a former
+        athlete, I have a 'grind' mindeset and love feedback because that is
+        where growth happens. No matter the problem, I will always find a way.
       </p>
     </div>
   );
