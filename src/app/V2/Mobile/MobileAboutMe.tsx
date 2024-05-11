2a63fb9574de79df67f396f0098f9bbb1b300b72
Chris Park <cp3063@columbia.edu>
2024-05-11 15:26:41 -0700

    Basic about me contents done

--- src/app/V2/Mobile/MobileAboutMe.tsx
+++ src/app/V2/Mobile/MobileAboutMe.tsx
@@ -1,12 +1,22 @@
-
-
+/* eslint-disable react/no-unescaped-entities */
 const MobileAboutMe = () => {
-
-    return (
-        <div className="flex flex-col mt-16">
-            <h1 className="text-white text-lg ">About Me</h1>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col mt-16 gap-6">
+      <h1 className="text-white text-lg ">About Me</h1>
+      <p className="text-gray-300">
+        I'm a software engineer with a knack for solving problems with software.
+        I have experience working with Next.js, TypeScript, Node.js, and more.
+        I'm always looking for new opportunities to learn and grow as a
+        developer.
+      </p>
+      <p className="text-gray-300">
+        My journey has been all over the place, from finance, to dentistry, but
+        ultimately I found my passion in software engineering. Being a former
+        athlete, I have a 'grind' mindeset and love feedback because that is
+        where growth happens. No matter the problem, I will always find a way.
+      </p>
+    </div>
+  );
+};
 
 export default MobileAboutMe;
